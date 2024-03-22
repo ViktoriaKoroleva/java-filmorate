198a92ce518dbf43d5909b2bd00fe6589284c9fd
viktoriakoroleva <koroleva_vs02@mail.ru>
2024-03-22 20:08:53 +0300

    fix12

--- src/main/java/ru/yandex/practicum/filmorate/controller/UserController.java
+++ src/main/java/ru/yandex/practicum/filmorate/controller/UserController.java
@@ -17,7 +17,7 @@ import java.util.Map;
 @RequestMapping("/users")
 @Validated
 public class UserController {
-    private final Map<Integer, User> users = new HashMap<>();
+    private final Map<Integer, User> userHashMap = new HashMap<>();
     private int userId = 1;
 
     private int generatorId() {
@@ -25,22 +25,16 @@ public class UserController {
     }
 
     @GetMapping
-    public Collection<User> getUsers() {
-        return users.values();
-    }
-
-    @GetMapping
     public List<User> findAll() {
-        log.info("Количество пользователей: {}", users.size());
-        return List.copyOf(users.values());
+        log.info("Количество пользователей: {}", userHashMap.size());
+        return List.copyOf(userHashMap.values());
     }
 
     @PostMapping
     public User createUser(@Valid @RequestBody User user) throws ValidationException {
 
-        validateUsers(user);
         user.setId(generatorId());
-        users.put(user.getId(), user);
+        userHashMap.put(user.getId(), user);
         log.info("Фильм успешно добавлен: {}", user);
 
         return user;
@@ -48,21 +42,13 @@ public class UserController {
 
     @PutMapping
     public User updateUser(@Valid @RequestBody User user) throws ValidationException {
-        validateUsers(user);
-        if (!users.containsKey(user.getId())) {
-            users.replace(user.getId(), user);
+        if (!userHashMap.containsKey(user.getId())) {
+            userHashMap.replace(user.getId(), user);
             log.debug("Пользователь успешно обновлен: {}", user);
         }
-        users.put(user.getId(), user);
+        userHashMap.put(user.getId(), user);
         log.info("Фильм c id" + user.getId() + " обновлён");
         return user;
     }
 
-    public User validateUsers(User user) throws ValidationException {
-        if (user.getName() == null || user.getName().isBlank()) {
-            log.info("Установлен логин вместо имени пользователя");
-            user.setName(user.getLogin());
-        }
-        return user;
-    }
 }
