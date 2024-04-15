31c902e33124a7f33ca2c1e03c4030f01baf52a4
viktoriakoroleva <koroleva_vs02@mail.ru>
2024-04-15 21:19:20 +0300

    sprint10-fix16

--- src/main/java/ru/yandex/practicum/filmorate/model/User.java
+++ src/main/java/ru/yandex/practicum/filmorate/model/User.java
@@ -11,7 +11,6 @@ import javax.validation.constraints.NotEmpty;
 import javax.validation.constraints.Past;
 import java.time.LocalDate;
 import java.util.List;
-import java.util.Set;
 
 @Data
 @AllArgsConstructor
