b716d7e2dbf423b2f7a2e046a074ecff32314f97
viktoriakoroleva <koroleva_vs02@mail.ru>
2024-04-17 21:53:01 +0300

    sprint10-fix33

--- src/main/java/ru/yandex/practicum/filmorate/model/User.java
+++ src/main/java/ru/yandex/practicum/filmorate/model/User.java
@@ -1,31 +1,35 @@
 package ru.yandex.practicum.filmorate.model;
 
-import lombok.AllArgsConstructor;
-import lombok.Builder;
-import lombok.Data;
-import lombok.NoArgsConstructor;
+import lombok.*;
 
-import javax.validation.constraints.Email;
-import javax.validation.constraints.NotBlank;
-import javax.validation.constraints.NotEmpty;
-import javax.validation.constraints.Past;
+import javax.validation.constraints.*;
 import java.time.LocalDate;
-import java.util.List;
+import java.util.HashSet;
+import java.util.Set;
 
-@Data
-@AllArgsConstructor
-@NoArgsConstructor
+@Getter
+@Setter
+@ToString
+@EqualsAndHashCode(of = {"id"})
 @Builder
 public class User {
-    private List<Integer> friends;
+
     private Integer id;
-    private String name;
-    @Past
-    private LocalDate birthday;
-    @NotEmpty
-    private String login;
-    @NotBlank(message = "Email не может быть пустым")
-    @Email(message = "Некорректный формат Email")
+
+    private final Set<Integer> friends = new HashSet<>();
+
+    @NotBlank(message = "Электронная почта не может быть пустой.")
+    @Email(message = "Неверный формат электронной почты.")
     private String email;
+
+    @NotBlank(message = "Логин не может быть пустым.")
+    @Pattern(regexp = "\\S+", message = "Логин не может содержать пробелы.")
+    private String login;
+
+    private String name;
+
+    @NotNull(message = "Дата рождения не может быть пустой.")
+    @Past(message = "Дата рождения не может быть в будущем.")
+    private LocalDate birthday;
 }
 
