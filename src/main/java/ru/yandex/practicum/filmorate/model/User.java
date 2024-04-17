f6fb8dfd758ad2e1d6cb0df91f5254d05ed33b21
viktoriakoroleva <koroleva_vs02@mail.ru>
2024-04-17 22:16:33 +0300

    sprint10-fix38

--- src/main/java/ru/yandex/practicum/filmorate/model/User.java
+++ src/main/java/ru/yandex/practicum/filmorate/model/User.java
@@ -23,7 +23,7 @@ public class User {
     private String email;
 
     @NotBlank(message = "Логин не может быть пустым.")
-    @Pattern(regexp = "\\S+", message = "Логин не может содержать пробелы.")
+    @Pattern(regexp = "\\S+", message = "Логин не может содержать пробелы")
     private String login;
 
     private String name;
