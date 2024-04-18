cc4e04680e704805d956cf575ac248c0519385ae
viktoriakoroleva <koroleva_vs02@mail.ru>
2024-04-18 06:46:25 +0300

    sprint10-fix45

--- src/main/java/ru/yandex/practicum/filmorate/validation/EntityNotFoundException.java
+++
@@ -1,7 +0,0 @@
-package ru.yandex.practicum.filmorate.validation;
-
-public class EntityNotFoundException extends NotFoundException {
-    public EntityNotFoundException(String message) {
-        super(message);
-    }
-}
---
+++ src/main/java/ru/yandex/practicum/filmorate/validation/handleNotFoundException.java
@@ -0,0 +1,7 @@
+package ru.yandex.practicum.filmorate.validation;
+
+public class handleNotFoundException extends NotFoundException {
+    public handleNotFoundException(String message) {
+        super(message);
+    }
+}
