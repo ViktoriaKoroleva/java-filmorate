f74fea57d933ed4b59f55131aa32cf1cf375c390
viktoriakoroleva <koroleva_vs02@mail.ru>
2024-06-05 21:43:33 +0300

    sprint11-fix2

--- src/main/java/ru/yandex/practicum/filmorate/model/Film.java
+++ src/main/java/ru/yandex/practicum/filmorate/model/Film.java
@@ -8,7 +8,6 @@ import org.springframework.validation.annotation.Validated;
 import javax.validation.constraints.*;
 import java.time.LocalDate;
 import java.util.List;
-import java.util.Set;
 
 @Data
 @Validated
