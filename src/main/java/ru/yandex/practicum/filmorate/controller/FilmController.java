bf171ab6bfd4883d499c054f31c3a8f899f4b7a8
viktoriakoroleva <koroleva_vs02@mail.ru>
2024-06-05 22:24:22 +0300

    sprint11-fix5

--- src/main/java/ru/yandex/practicum/filmorate/controller/FilmController.java
+++ src/main/java/ru/yandex/practicum/filmorate/controller/FilmController.java
@@ -5,10 +5,8 @@ import lombok.extern.slf4j.Slf4j;
 import org.springframework.web.bind.annotation.*;
 import ru.yandex.practicum.filmorate.model.Film;
 import ru.yandex.practicum.filmorate.service.film.FilmService;
-import ru.yandex.practicum.filmorate.validation.ValidationException;
 
 import javax.validation.Valid;
-import java.time.LocalDate;
 import java.util.List;
 
 @Slf4j
@@ -18,11 +16,9 @@ import java.util.List;
 public class FilmController {
 
     private final FilmService filmService;
-    private final static LocalDate START_RELEASE_DATE = LocalDate.of(1895, 12, 28);
 
     @PutMapping
     public Film update(@Valid @RequestBody Film film) {
-        film = validate(film);
         return filmService.update(film);
     }
 
@@ -59,13 +55,5 @@ public class FilmController {
     @GetMapping("/popular")
     public List<Film> getPopularFilms(@RequestParam(required = false) Long count) {
         return filmService.getPopularFilms(count);
-    }
-
-    public Film validate(Film data) {
-        if (data.getReleaseDate().isBefore(START_RELEASE_DATE)) {
-            log.warn("Release date film is before 28.12.1895");
-            throw new ValidationException("Film release date is invalid");
-        }
-        return data;
     }
 }
