64d3285784e875299e85d4613e20d0b9820c4e99
viktoriakoroleva <koroleva_vs02@mail.ru>
2024-06-05 21:03:56 +0300

    sprint11-fix1

--- src/main/java/ru/yandex/practicum/filmorate/controller/GenreController.java
+++ src/main/java/ru/yandex/practicum/filmorate/controller/GenreController.java
@@ -2,7 +2,6 @@ package ru.yandex.practicum.filmorate.controller;
 
 import lombok.AllArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
-import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -17,7 +16,6 @@ import java.util.List;
 @RequestMapping("/genres")
 @AllArgsConstructor
 public class GenreController {
-
     private final GenreService service;
 
     @GetMapping
