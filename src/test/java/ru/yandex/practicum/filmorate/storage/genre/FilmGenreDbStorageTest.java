d5e110e67d6a012ee987169c1166b69fd18cb387
viktoriakoroleva <koroleva_vs02@mail.ru>
2024-06-08 23:44:31 +0300

    sprint11-fix21

--- src/test/java/ru/yandex/practicum/filmorate/storage/genre/FilmGenreDbStorageTest.java
+++ src/test/java/ru/yandex/practicum/filmorate/storage/genre/FilmGenreDbStorageTest.java
@@ -8,7 +8,6 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.jdbc.JdbcTest;
 import org.springframework.jdbc.core.JdbcTemplate;
 import ru.yandex.practicum.filmorate.model.Film;
-import ru.yandex.practicum.filmorate.model.Genre;
 import ru.yandex.practicum.filmorate.storage.film.FilmDbStorage;
 import ru.yandex.practicum.filmorate.storage.mpa.MpaDbStorage;
 
